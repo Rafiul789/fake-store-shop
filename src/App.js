9a359f9f2532f11cf5e14da2777d8e250fb72dcd
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-03-19 11:06:42 +0600

    button added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Menubar from './Menubar/Menubar';
+import Test from './Test/Test';
 function App() {
+
+  const count=()=>{
+    console.log('hello paisi brooo')
+  }
   return (
     <div className="App">
      <Menubar country="Uganda"  ></Menubar>
+     <h1>0</h1>
+     <Test count={count} ></Test>
     </div>
   );
 }
--- src/Test/Test.js
+++ src/Test/Test.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const Test = (props) => {
+   
+
+const [count,setCount]=useState(0);
+
+console.log(count)
     return (
         <div>
-            <h1>He is from {props.country}</h1>
+            
+            <div  className="count  mt-5">   <h1>{count}</h1>
+            <button  onClick={()=>setCount(count+1)}  className="ms-3 p-3">+</button><button onClick={()=>setCount(count-1)}  className="ms-3 p-3"  > - </button></div>
         </div>
     );
 };
