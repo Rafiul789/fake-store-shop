3ab6eefd491091c1c0204117fba6d3ecdd1f4cc9
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-03-19 12:57:26 +0600

    menubar added

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Menubar from './Menubar/Menubar';
-import Test from './Test/Test';
+
 function App() {
 
-  const count=()=>{
-    console.log('hello paisi brooo')
-  }
-  return (
+ 
+    
+    return (
     <div className="App">
-     <Menubar country="Uganda"  ></Menubar>
-     <h1>0</h1>
-     <Test count={count} ></Test>
+    <Menubar></Menubar>
     </div>
   );
 }
--- src/Menubar/Menubar.js
+++ src/Menubar/Menubar.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import Test from '../Test/Test';
+import "./Menubar.css"
 
-const Menubar = ({country}) => {
-    console.log(country);
+const Menubar = () => {
+    
     return (
         <div>
-            <h1>Hero Alom from menubar</h1>
-            <Test country={country} ></Test>
+            <h1>Menubar</h1> <div className="container">
+            <div className="row"> <div className="col-md-2 p-2">logo</div>
+            <div className="col-md-10 ms-4 menu-container d-flex justify-content-around-end ">         <li className="item ms-4 mb-4">Home</li>
+            <li className="item ms-4 mb-4">Contact</li>
+            <li className="item ms-4 mb-4">Cart</li>
+            <li className="item ms-4 mb-4">Login</li>  </div>  </div>
+            </div>
+           
+          
         </div>
     );
 };
